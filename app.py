cafda36e9ca43545a3cdee7ef52ea0ea41024db3
Abdul Mannan <abdul.mannan@catchprobe.com>
2022-01-10 13:45:39 +0500

    Bug Fixes
    Bug Fixes

---
+++ app.py
@@ -0,0 +1,5 @@
+from cronjob_manager.application_controller.application_controller import application_controller
+from cronjob_manager.application_controller.application_enums import APPICATION_CONTROLLER_COMMANDS
+
+application_controller.get_instance().invoke_trigger(APPICATION_CONTROLLER_COMMANDS.S_CREATE_APPLICATION_STATUS)
+application_controller.get_instance().invoke_trigger(APPICATION_CONTROLLER_COMMANDS.S_START_APPLICATION)
