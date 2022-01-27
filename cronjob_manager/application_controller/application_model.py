f0642c3727106f1c51fe5f25160f44ae5536bd1f
Abdul Mannan <abdul.mannan@catchprobe.com>
2022-01-27 23:59:00 +0500

    Bug Fixes
    Bug Fixes

--- cronjob_manager/application_controller/application_model.py
+++ cronjob_manager/application_controller/application_model.py
@@ -1,16 +1,18 @@
 import threading
 import warnings
 
+import requests
+
+from cronjob_manager.constants.constant import constants
 from cronjob_manager.jobs.cleanup_controller.cleanup_manager.cleanup_controller import cleanup_controller
 from native_services.services.mongo_manager.mongo_controller import mongo_controller
 from native_services.services.mongo_manager.mongo_enums import MONGO_CRUD, MONGODB_COMMANDS
-from time import sleep
-from log_manager.log_controller import log
 from cronjob_manager.constants.cronjob_commands import BACKUP_CONTROLLER_COMMANDS, CLEANUP_CONTROLLER_COMMANDS
 from cronjob_manager.jobs.backup_controller.backup_manager.backup_controller import backup_controller
-from cronjob_manager.application_controller.application_enums import APPICATION_MODEL_COMMANDS, CRONJOB_COMMANDS_TIMER, \
-    CRONJOB_MESSAGES
+from cronjob_manager.application_controller.application_enums import APPICATION_MODEL_COMMANDS, CRONJOB_COMMANDS_TIMER, CRONJOB_MESSAGES
 from cronjob_manager.shared_model.request_handler import request_handler
+from time import sleep
+from log_manager.log_controller import log
 
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
@@ -28,6 +30,9 @@ class application_model(request_handler):
     def __start_cleanup(self):
         cleanup_controller.get_instance().invoke_trigger(CLEANUP_CONTROLLER_COMMANDS.S_START_CLEANUP)
 
+    def __update_status(self):
+        requests.get(constants.S_UPDATE_STATUS_URL, timeout=10)
+
     def __init_cronjob(self):
         while True:
             try:
@@ -38,8 +43,11 @@ class application_model(request_handler):
                    self.__start_cleanup()
                 if self.__m_timer % CRONJOB_COMMANDS_TIMER.S_CREATE_BACKUP == 0:
                    self.__create_backup()
+                if self.__m_timer % CRONJOB_COMMANDS_TIMER.S_UPDATE_STATUS == 0:
+                   threading.Thread(target=self.__update_status).start()
+
                 if self.__m_timer % CRONJOB_COMMANDS_TIMER.S_PING == 0:
-                    log.g().i("CRONJOB : " + CRONJOB_MESSAGES.S_ALIVE)
+                   log.g().i("CRONJOB : " + CRONJOB_MESSAGES.S_ALIVE)
 
             except Exception as ex:
                 log.g().e("CRONJOB 1 : " + str(ex))
---
+++ cronjob_manager/constants/constant.py
@@ -0,0 +1,2 @@
+class constants:
+    S_UPDATE_STATUS_URL = "http://localhost:8000/update_status/?pRequest=m_cronjob"
